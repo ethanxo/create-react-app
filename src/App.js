10784419452d3740fc4f3df0949fdf371901c2be
ethan <51106797+ethanxo@users.noreply.github.com>
2023-02-08 13:41:43 -0500

    init

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+      <div class="controls">
+      <button id="cardnew" class="button">add clone card</button>
+      <button id="cardtitle" class="button">change clone title</button>
+      <button id="cardbg" class="button">toggle clone bg color</button>
+      <button id="carddel" class="button">delete newest clone</button>
     </div>
+    <div id="cards" class="cards">
+      <div id="card" class="card">
+        <img src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=" id="image" />
+        <h2 id="title">Placeholder Card</h2>
+        <p id="description">placeholder card description</p>
+        <button id="details" class="button">details</button>
+      </div>
+    </div>
+  </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,47 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //const linkElement = screen.getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+  const btn_cardnew = document.getElementById('cardnew');
+const btn_cardtitle = document.getElementById('cardtitle');
+const btn_cardbg = document.getElementById('cardbg');
+const btn_carddel = document.getElementById('carddel');
+const btn_details = document.getElementById('details');
+
+btn_cardnew.addEventListener("click", (e) => { 
+  let clone = document.getElementById('card').cloneNode(true);
+  clone.classList.add('dupecard');
+  document.getElementById('cards').appendChild(clone);
+  let btn_clonedetails = clone.children[3];
+  btn_clonedetails.addEventListener("click", (e) => { 
+    let desc = btn_clonedetails.previousElementSibling;
+    (desc.style.display == 'none') ? (desc.style.display = '') : (desc.style.display = 'none');
+  });
+});
+btn_cardtitle.addEventListener("click", (e) => { 
+  if (document.querySelectorAll('.dupecard') != null) {
+    index = prompt("Enter the index of the CLONE you wish to edit")
+    title = prompt("Enter new title for the card");
+    let clone = document.querySelectorAll('.dupecard')[index];
+    clone.children[1].innerHTML = title;
+  }
+});
+btn_cardbg.addEventListener("click", (e) => { 
+  if (document.querySelectorAll('.dupecard') != null) {
+    index = prompt("Enter the index of the CLONE you wish to edit")
+    color = prompt("Enter a color such as red, or green or white to return to default");
+    let clone = document.querySelectorAll('.dupecard')[index];
+    clone.style.backgroundColor = color;
+  }
+});
+btn_carddel.addEventListener("click", (e) => { 
+  if (document.querySelectorAll('.dupecard') != null) {
+    let dupecards = document.querySelectorAll('.dupecard'); document.getElementById('cards').removeChild(dupecards[dupecards.length - 1]);
+  }
+});
+btn_details.addEventListener("click", (e) => { 
+  let desc = btn_details.previousElementSibling;
+  (desc.style.display == 'none') ? (desc.style.display = '') : (desc.style.display = 'none');
+});
 });
